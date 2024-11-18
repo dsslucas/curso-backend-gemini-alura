5957f05b0194d5e267ef1c553999627f7e7e01e4
dsslucas <lucasdasilvasou@gmail.com>
2024-11-18 14:33:29 -0300

    feat: lesson

--- server.js
+++ server.js
@@ -11,3 +11,13 @@ app.listen(port, () => {
 app.get("/api", (req, res) => {
     res.status(200).send("A Torre Eiffel iluminada à noite, com milhares de luzes cintilando, criando um espetáculo mágico em Paris.");
 });
+
+// Exercise
+app.get("/livro", (req, res) => {
+    res.status(200).send({
+        "titulo": "O Senhor dos Anéis",
+        "autor": "J.R.R. Tolkien",
+        "ano": 1954,
+        "genero": "Fantasia"
+    });
+});
