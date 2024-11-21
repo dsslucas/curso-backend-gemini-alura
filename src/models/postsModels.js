f5be15db205b9af2e6dad737b118bfa31dcdca7f
dsslucas <lucasdasilvasou@gmail.com>
2024-11-20 22:08:13 -0300

    fix: adjusting mongodb search

--- src/models/postsModels.js
+++ src/models/postsModels.js
@@ -4,22 +4,22 @@ import connectingDatabase from "../config/database.js";
 const connectDatabase = await connectingDatabase(process.env.KEY_MONGODB);
 
 const postsModels = () => {
-    const getAllPosts = async () => {        
+    const getAllPosts = async () => {
         const db = connectDatabase.db("imersao-instabytes");
-        const collection = db.collection("posts");    
+        const collection = db.collection("posts");
         return collection.find().toArray();
     }
 
     const getPostById = async (id) => {
         const db = connectDatabase.db("imersao-instabytes");
         const collection = db.collection("posts");
-        return collection.findOne({ _id: id }, { projection: { title: 1, content: 1 } });
+        return collection.findOne({ _id: new ObjectId(id) });
     }
 
     const getPostByDescription = async (description) => {
         const db = connectDatabase.db("imersao-instabytes");
         const collection = db.collection("posts");
-        await collection.find({ descricao: description }).toArray();
+        await collection.findOne({ descricao: description });
     }
 
     return {
